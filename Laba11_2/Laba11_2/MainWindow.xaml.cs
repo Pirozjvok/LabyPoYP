fc6d1ab58b5b97af71d1eee5e42c5b353c93d898
AynurV <acauntd721@gmail.com>
2023-10-29 04:20:24 +0500

    Laba11_2 done. I hate this lab. All laboratory tests have been done. Finally lets go!

--- Laba11_2/Laba11_2/MainWindow.xaml.cs
+++ Laba11_2/Laba11_2/MainWindow.xaml.cs
@@ -23,6 +23,12 @@ namespace Laba11_2
         public MainWindow()
         {
             InitializeComponent();
+            image1.textBox.Text = "https://postila.ru/data/1e/69/83/7d/1e69837d2677676a2dd64af78393010b9db85c2a6b9acebed9eba21c8f0ba3ee.jpg";
+            image2.textBox.Text = "http://rasfokus.ru/images/photos/medium/a187117ea1d3d38f2d1437ad86f2602a.jpg";
+            image3.textBox.Text = "https://avatars.mds.yandex.net/i?id=2afa1dcc0bcea8a61e298dc579d1181cf972a5c9-10576312-images-thumbs&n=13";
+            image4.textBox.Text = "https://postila.ru/data/1e/69/83/7d/1e69837d2677676a2dd64af78393010b9db85c2a6b9acebed9eba21c8f0ba3ee.jpg";
+            image5.textBox.Text = "http://rasfokus.ru/images/photos/medium/a187117ea1d3d38f2d1437ad86f2602a.jpg";
+            image6.textBox.Text = "https://avatars.mds.yandex.net/i?id=2afa1dcc0bcea8a61e298dc579d1181cf972a5c9-10576312-images-thumbs&n=13";
         }
     }
 }
